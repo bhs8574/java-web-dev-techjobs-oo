e662217ce63219547b7a11044c1580c96dd9b702
bhs8574 <bhs8574@gmail.com>
2020-05-10 17:31:44 -0500

    Completed through Section 4 to start TDD

---
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -0,0 +1,47 @@
+package org.launchcode.techjobs_oo.Tests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.launchcode.techjobs_oo.*;
+
+import static org.junit.Assert.*;
+
+public class JobTest {
+
+    Job job1;
+    Job job2;
+    Job job3;
+    Job job4;
+
+    @Before
+    public void createJobs() {
+        job1 = new Job();
+        job2 = new Job();
+        job3 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        job4 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+    }
+
+    @Test
+    public void testSettingJobId() {
+        assertEquals(job1.getId()+1, job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertEquals(job2.getId()+1, job3.getId());
+        assertTrue(job3.getName() instanceof String);
+        assertTrue(job3.getEmployer() instanceof Employer);
+        assertTrue(job3.getLocation() instanceof Location);
+        assertTrue(job3.getPositionType() instanceof PositionType);
+        assertTrue(job3.getCoreCompetency() instanceof CoreCompetency);
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        assertFalse(job3.equals(job4));
+    }
+
+
+
+
+}
