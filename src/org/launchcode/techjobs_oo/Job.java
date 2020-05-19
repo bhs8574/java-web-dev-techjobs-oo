6152689bb3360fd292272ba3113b6f149c9fa9a6
bhs8574 <bhs8574@gmail.com>
2020-05-18 19:56:09 -0500

    Commented up my Job and JobField classes.

--- src/org/launchcode/techjobs_oo/Job.java
+++ src/org/launchcode/techjobs_oo/Job.java
@@ -17,11 +17,26 @@ public class Job {
     //  other five fields. The second constructor should also call the first in order to initialize
     //  the 'id' field.
 
+    /**
+     Constructor to assign the current "nextId" to the id field and
+     increment the nextId variable by 1 so the next question created
+     receives the correct id.
+     */
     public Job() {
         this.id = nextId;
         nextId++;
     }
 
+
+    /**
+     * Constructor to set the rest of the fields in each job. Also calls the above constructor to
+     * handle id initialization.
+     * @param aName a String for the job's name.
+     * @param aEmployer an Employer object
+     * @param aLocation a Location object
+     * @param aPositionType a PositionType object
+     * @param aCoreCompetency a CoreCompetency object
+     */
     public Job(String aName, Employer aEmployer, Location aLocation, PositionType aPositionType, CoreCompetency aCoreCompetency) {
         this();
         this.name = aName;
@@ -31,14 +46,28 @@ public class Job {
         this.coreCompetency = aCoreCompetency;
     }
 
+
+    /**
+     * Converts a Job object to a formatted block of text with one line per field, or if each field is null,
+     * returns a message that the job does not exist.
+     * @return a formatted String of the data in this job.
+     */
     @Override
     public String toString() {
+        String tempName = this.getName();
+
+        //If all Job fields are null, then return the does not exist string.
         if (this.name == null && this.employer == null && this.location == null && this.positionType == null && this.coreCompetency == null) {
             return "OOPS! This job does not seem to exist.";
         }
+        //Checks to see if Name is missing or empty, and if so, assigns "Data not available" to tempName to be returned
+        if (this.getName() == null || this.getName().trim().equals("")) {
+            tempName = "Data not available";
+        }
+        // returns the formatted block of code using each field's toString() when possible.
         return "\n" +
                 "ID: " + this.getId() + "\n" +
-                "Name: " + this.getName() + "\n" +
+                "Name: " + tempName + "\n" +
                 "Employer: " + this.getEmployer().toString() + "\n" +
                 "Location: " + this.getLocation().toString() + "\n" +
                 "Position Type: " + this.getPositionType().toString() + "\n" +
@@ -49,6 +78,7 @@ public class Job {
     // TODO: Add custom equals and hashCode methods. Consider two Job objects "equal" when their id fields
     // match.
 
+    // IntelliJ generated equals method that compares the id field only.
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -65,46 +95,87 @@ public class Job {
     // TODO: Add getters for each field EXCEPT nextId. Add setters for each field EXCEPT nextID
     //  and id.
 
+    /**
+    @return this job's int id
+     */
     public int getId() {
         return id;
     }
 
+    /**
+    @return name String for this job
+     */
     public String getName() {
         return name;
     }
 
+    /**
+    @param name String to set as this job's name.
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return the Employer object for this job
+     */
     public Employer getEmployer() {
         return employer;
     }
 
+    /**
+     *
+     * @param employer the Employer object to assign the employer field for this job
+     */
     public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
+    /**
+     *
+     * @return the location object for this job
+     */
     public Location getLocation() {
         return location;
     }
 
+    /**
+     *
+     * @param location the Location object to assign the location field for this job
+     */
     public void setLocation(Location location) {
         this.location = location;
     }
 
+    /**
+     *
+     * @return the PositionType object for this job
+     */
     public PositionType getPositionType() {
         return positionType;
     }
 
+    /**
+     *
+     * @param positionType the PositionType object to assign the positionType field for this job
+     */
     public void setPositionType(PositionType positionType) {
         this.positionType = positionType;
     }
 
+    /**
+     *
+     * @return the CoreCompetency object for this job
+     */
     public CoreCompetency getCoreCompetency() {
         return coreCompetency;
     }
 
+    /**
+     *
+     * @param coreCompetency the CoreCompetency object to assign the coreCompetency field for this job.
+     */
     public void setCoreCompetency(CoreCompetency coreCompetency) {
         this.coreCompetency = coreCompetency;
     }
--- src/org/launchcode/techjobs_oo/JobField.java
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -7,16 +7,28 @@ public abstract class JobField {
     private static int nextId = 1;
     private String value;
 
+    /**
+     * give each JobField a unique id like each job, so they can be compared easily.
+     */
     public JobField() {
         id = nextId;
         nextId++;
     }
 
+    /**
+     *
+     * @param value a String to hold the data for the field.
+     */
     public JobField(String value) {
         this();
         this.value = value;
     }
 
+    /**
+     *
+     * @return either the value String with the field data, or the string
+     * "Data not available" if value is empty or null.
+     */
     @Override
     public String toString() {
         if (value == null || value.trim().equals("")) {
@@ -25,6 +37,9 @@ public abstract class JobField {
         return value;
     }
 
+    /*
+    Two JobFields are equal if they hold the same id.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,14 +53,26 @@ public abstract class JobField {
         return Objects.hash(id);
     }
 
+    /**
+     *
+     * @return the int id of this JobField
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     *
+     * @return the String value holding the data for this JobField
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     *
+     * @param value the String to set value to for this JobField.
+     */
     public void setValue(String value) {
         this.value = value;
     }
